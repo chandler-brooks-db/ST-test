7f2a0998ec454d156a2f615dbfede4905ce243c9
chandler-brooks-db <107142389+chandler-brooks-db@users.noreply.github.com>
2023-10-16 18:58:04 -0400

    Update cb_test_table_1.sql

--- my_ddls/cb_test_table_1.sql
+++ my_ddls/cb_test_table_1.sql
@@ -1,6 +1,4 @@
--- Databricks notebook source
-%sql
 CREATE TABLE IF NOT EXISTS cb_demos.bronze.cb_test_table_1 (
   customer_id STRING,
   tax_id STRING,
-  tax_code STRING) USING delta PARTITIONED BY (tax_id)
+  tax_code STRING) USING delta PARTITIONED BY (tax_id);
