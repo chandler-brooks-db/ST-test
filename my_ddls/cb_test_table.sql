dbc24834c0f20c95c82499af55501d2163e3e53f
chandler-brooks-db <107142389+chandler-brooks-db@users.noreply.github.com>
2023-10-16 12:58:57 -0400

    Update cb_test_table.sql

--- my_ddls/cb_test_table.sql
+++ my_ddls/cb_test_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS cb_demos.bronze.cb_test_table (
+CREATE TABLE IF NOT EXISTS cb_demos.bronze.cb_test_table_1 (
   customer_id STRING,
   tax_id STRING,
   tax_code STRING) USING delta PARTITIONED BY (tax_id)
